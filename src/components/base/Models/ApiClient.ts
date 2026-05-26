9c1d5db532a37fb50cab102dd988f504892fd95f
oll-soll <ollsollina@gmail.com>
2026-05-26 18:24:07 +0500

    feat: class ApiClient added

---
+++ src/components/base/Models/ApiClient.ts
@@ -0,0 +1,17 @@
+import { IApi, IProductsResponse, IOrderRequest, IOrderResponse } from "../../../types";
+
+export class ApiClient {
+    protected api: IApi;
+
+    constructor(api: IApi) {
+        this.api = api;
+    }
+
+    getProducts(): Promise<IProductsResponse> {
+        return this.api.get<IProductsResponse>('/product/');
+    }
+
+    postOrder(data: IOrderRequest): Promise<IOrderResponse> {
+        return this.api.post<IOrderResponse>('/order/', data);
+    }
+}
