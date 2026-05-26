1434cf8629b78c9f73de891ea09db04899e75ec4
oll-soll <ollsollina@gmail.com>
2026-05-26 15:52:55 +0500

    test: checking class implementation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
+import { CatalogModel } from './components/base/Models/CatalogModel';
 import './scss/styles.scss';
+import { apiProducts } from './utils/data';
+import { BasketModel } from './components/base/Models/BasketModel';
+import { BuyerModel } from './components/base/Models/BuyerModel';
+
+const productsModel = new BuyerModel();
+productsModel.validate();
+
+console.log("Ошибки формы: ", productsModel.validate())
